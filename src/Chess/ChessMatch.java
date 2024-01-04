e01f76e36b76458122587dcfbf7b7819b535c5f3
Gustavo <gemidio2004@gmail.com>
2024-01-04 18:07:40 -0300

    Possible moves of a piece

--- src/Chess/ChessMatch.java
+++ src/Chess/ChessMatch.java
@@ -2,8 +2,8 @@ package Chess;
 
 import boardgame.Board;
 import boardgame.BoardException;
-	import boardgame.Piece;
-	import boardgame.Position;
+import boardgame.Piece;
+import boardgame.Position;
 import Chess.pieces.King;
 import Chess.pieces.Rook;
 
@@ -38,6 +38,9 @@ public class ChessMatch {
 	private void validateSourcePosition(Position position) {
 		if(!board.thereIsAPiece(position)) {
 			throw new ChessException("There is no piece on source position");
+		}
+		if(!board.piece(position).isThereAnyPossibleMove()) {
+			throw new ChessException("There is no possible moves for the chosen piece"); 
 		}
 	}
 	
--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,6 +1,6 @@
 package boardgame;
 
-public class Piece {
+public abstract class Piece {
 	protected Position position; 
 	private Board board;
 	
@@ -12,6 +12,32 @@ public class Piece {
 	protected Board getBoard() {
 		return board;
 	} 
+	
+	// MÉTODOS: 
+	
+	public abstract boolean[][] possibleMoves();
+	
+	// VERIFICA SE É POSIÇÃO A PEÇA SER MOVIDA PARA DADA POSIÇÃO: 
+	public boolean possibleMove(Position position) {
+		return possibleMoves()[position.getRow()][position.getColumn()]; 
+	}
+	
+	// VERIFICA SE EXISTE PELO MENOS 1 MOVIMENTO POSSÍVEL PARA DETERMINADA PEÇA: 
+	public boolean isThereAnyPossibleMove(){
+		boolean[][] mat=possibleMoves(); 
+		for(int i=0;i<mat.length;i++) {
+			for(int j=0;j<mat.length;j++) {
+				if(mat[i][j]==true) {
+					return true;
+				}
+			}
+		}
+		return false; 
+	}
+
+	
+
+
 	
 	
 	
