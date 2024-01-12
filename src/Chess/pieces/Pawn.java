7596176f08bf246ac702639a01dc56ef12340468
Gustavo <gemidio2004@gmail.com>
2024-01-12 09:40:47 -0300

    Special move - En Passant

--- src/Chess/pieces/Pawn.java
+++ src/Chess/pieces/Pawn.java
@@ -1,17 +1,20 @@
 package Chess.pieces;
 
+import Chess.ChessMatch;
 import Chess.ChessPiece;
 import Chess.Color;
 import boardgame.Board;
 import boardgame.Position;
 
 public class Pawn extends ChessPiece{
-
+	private ChessMatch chessMatch; 
 	
-	public Pawn(Board board, Color color) {
+	public Pawn(Board board, Color color, ChessMatch chessMatch) {
 		super(board, color);
+		this.chessMatch=chessMatch; 
 	}
 	
+	// MOVIMENTOS POSSÍVEIS DO PEÃO: 
 	@Override
 	public boolean[][] possibleMoves() {
 		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
@@ -19,7 +22,7 @@ public class Pawn extends ChessPiece{
 		Position p = new Position(0, 0);
 		
 		//PEÃO BRANCO: 
-		if(getColor()==getColor().WHITE) {
+		if(getColor()==getColor().WHITE){
 			p.setValues(position.getRow()-1, position.getColumn());
 			// SE A POSIÇÃO EXISTIR E NÃO TIVER UMA PEÇA NELA, O PEÃO PODE MOVER PRA LÁ:
 			if(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)){
@@ -44,7 +47,33 @@ public class Pawn extends ChessPiece{
 			if(getBoard().positionExists(p) && isThereOpponentPiece(p)){
 				mat[p.getRow()][p.getColumn()]=true;  
 			}
-		// MESMO RACIOCINIO SÓ QUE AGORA PARA A PEÇA PRETA
+			
+			// EN PASSANT-PEÇA BRANCA: 
+			
+			//(LADO ESQUERDO):
+			
+			// VERIFICANDO SE ESTÁ NA ÚNICA POSIÇÃO POSSIVEL PARA SE FAZER A JOGADA: 	
+			if(position.getRow()==3){
+				Position left=new Position(position.getRow(),position.getColumn()-1);
+				// VERIFICANDO SE A POSIÇÃO EXISTE, É UM OPONENTE, E ESTÁ VULNERAVEL: 
+				if(getBoard().positionExists(left) && isThereOpponentPiece(left)
+				   && getBoard().piece(left)==chessMatch.getEnPassantVulnerable()){
+					// ATENDENDO A VERIFICAÇÃO O PEÃO PODE SER MOVIDO PARA A DIAGONAL ESQUERDA:
+					mat[left.getRow()-1][left.getColumn()]=true; 
+				}
+			}
+			// LADO DIRETO:
+	
+			// VERIFICANDO SE ESTÁ NA ÚNICA POSIÇÃO POSSIVEL PARA SE FAZER A JOGADA: 	
+				Position right=new Position(position.getRow(),position.getColumn()+1);
+				// VERIFICANDO SE A POSIÇÃO EXISTE, É UM OPONENTE, E ESTÁ VULNERAVEL: 
+				if(getBoard().positionExists(right) && isThereOpponentPiece(right)
+					&& getBoard().piece(right)==chessMatch.getEnPassantVulnerable()){
+					// ATENDENDO A VERIFICAÇÃO O PEÃO PODE SER MOVIDO PARA A DIAGONAL DIREITA:
+					mat[right.getRow()-1][right.getColumn()]=true; 
+				}
+			
+		// MESMO RACIOCINIO SÓ QUE AGORA PARA A PEÇA PRETA:
 		}else{
 			p.setValues(position.getRow()+1, position.getColumn());
 			// SE A POSIÇÃO EXISTIR E NÃO TIVER UMA PEÇA NELA, O PEÃO PODE MOVER PRA LÁ:
@@ -70,10 +99,34 @@ public class Pawn extends ChessPiece{
 			if(getBoard().positionExists(p) && isThereOpponentPiece(p)){
 				mat[p.getRow()][p.getColumn()]=true;  
 			}
-		}
-		return mat;
-	}
+			
+			// EN PASSANT-PEÇA PRETA: 
+			
+			//LADO ESQUERDO:
+			
+			// VERIFICANDO SE ESTÁ NA ÚNICA POSIÇÃO POSSIVEL PARA SE FAZER A JOGADA: 	
+			if(position.getRow()==4){
+				Position left=new Position(position.getRow(),position.getColumn()-1);
+				// VERIFICANDO SE A POSIÇÃO EXISTE, É UM OPONENTE, E ESTÁ VULNERAVEL: 
+				if(getBoard().positionExists(left) && isThereOpponentPiece(left)
+				   && getBoard().piece(left)==chessMatch.getEnPassantVulnerable()){
+					mat[left.getRow()+1][left.getColumn()]=true; 
+				}
+			}
+			// LADO DIRETO:
 	
+			// VERIFICANDO SE ESTÁ NA ÚNICA POSIÇÃO POSSIVEL PARA SE FAZER A JOGADA: 	
+				Position right=new Position(position.getRow(),position.getColumn()+1);
+				// VERIFICANDO SE A POSIÇÃO EXISTE, É UM OPONENTE, E ESTÁ VULNERAVEL: 
+				if(getBoard().positionExists(right) && isThereOpponentPiece(right)
+					&& getBoard().piece(right)==chessMatch.getEnPassantVulnerable()){
+					mat[right.getRow()+1][right.getColumn()]=true; 
+				}
+		}	
+		
+	return mat;
+	}	
+		
 	@Override
 	public String toString() {
 		return "P";
